baba1754b22ba547c7f3614457ab3f9295d46d29
Ann <fullsokan@gmail.com>
2022-07-02 17:28:22 +0300

    debouncer v.2.3. (improved rtl logic)

--- debouncer/rtl/debouncer.sv
+++ debouncer/rtl/debouncer.sv
@@ -9,15 +9,13 @@ module debouncer #(
   output logic key_pressed_stb_o
 );
 
-logic d1;
-logic d2;
-logic flag;
-
-logic [$clog2(GLITCH_TIME_NS):0] counter = '1;
+logic                            d1;
+logic                            d2;
+logic                            flag;
+logic [$clog2(GLITCH_TIME_NS):0] counter;
 
 localparam CLK_PERIOD_NS = 1000 / CLK_FREQ_MHZ;
 localparam WAIT_HW_TICK  = GLITCH_TIME_NS / CLK_PERIOD_NS;
-
 
 // d1 logic
 always_ff @( posedge clk_i )
@@ -37,16 +35,18 @@ always_ff @( posedge clk_i )
     if( d1 != d2 )
       counter <= '0;
     else
-      if( counter != '1 )
-        if( counter == ( WAIT_HW_TICK - 2) )
-          counter <= '1;
-        else
-          counter <= counter + ($clog2(GLITCH_TIME_NS))'(1);
+      if( counter < WAIT_HW_TICK - 1 )
+        counter <= counter + ($clog2(GLITCH_TIME_NS))'(1);
   end
 
-assign flag = ( counter == ( WAIT_HW_TICK - 2) );
+assign flag = ( counter == ( WAIT_HW_TICK - 2 ) );
 
-assign key_pressed_stb_o = ( flag && d1 == d2 );
+always_comb
+  begin
+    if( flag && ( d1 == d2 ) )
+      key_pressed_stb_o = 1'b1;
+    else
+      key_pressed_stb_o = 1'b0;
+  end
 
 endmodule
-
